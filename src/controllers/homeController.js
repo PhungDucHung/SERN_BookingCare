852f911970e455cf8756973be5e71bbb22ae7681
Phung Duc Hung <phungduchung9808@gmail.com>
2024-07-01 04:38:02 +0700

    Use ORM Insert new  Data for Database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from '../models/index';
+import CRUDService from '../services/CRUDService';
 
 let getHomePage = async(req, res) => {
     try{
@@ -15,11 +16,20 @@ let getAboutPage = (req, res) => {
     return res.render('test/about.ejs');
 }
 
-// object: {
-//     key: '',
-//     value: ''
-// }
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+}
+
+let postCRUD = async(req, res) => {
+    let message = await CRUDService.createNewUser(req.body)
+    return res.send('post crud from server');
+}
+
+
+
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage: getAboutPage
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
 }
