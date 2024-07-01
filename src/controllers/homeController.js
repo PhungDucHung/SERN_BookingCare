f25fcc73ff899429e8927bedd63b0f2011c1f324
Phung Duc Hung <phungduchung9808@gmail.com>
2024-07-02 02:07:10 +0700

    Update Data with Sequelize

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -30,10 +30,33 @@ let displayGetCRUD = async(req, res) => {
     });
 }
 
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if(userId){
+        let userData = await CRUDService.getUserInfoById(userId)
+        return res.render('editCRUD.ejs',{
+            user: userData
+        })
+    }
+    else{
+        return res.send('user not found')
+    }
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body;  // req.body lấy giá trị input
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render('displayCRUD.ejs',{
+        // truyền data sang views - nhiều dữ liệu dùng loop
+        data: allUsers
+    });
+}
+
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage: getAboutPage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD : displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD
 }
