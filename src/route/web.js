beca2d6305acf723df047023540a6cf1545bb36d
Phung Duc Hung <phungduchung9808@gmail.com>
2024-07-06 02:49:23 +0700

    Write API Get All Info User

--- src/route/web.js
+++ src/route/web.js
@@ -16,8 +16,11 @@ let initWebRoutes = (app) => {
     router.get('/delete-crud', homeController.deleteCRUD);
 
     router.post('/api/login' , userController.handleLogin);
+    router.get('/api/get-all-users', userController.handleGetAllUsers);
+
 
     return app.use("/", router);
+
 }
 
 module.exports = initWebRoutes;
--- src/services/userService.js
+++ src/services/userService.js
@@ -5,11 +5,11 @@ import bcrypt from 'bcryptjs';
 let handleUserLogin = (userEmail, password) => {
     return new Promise(async (resolve, reject) => {
         try {
-            let userData = {};
+            let userData = {}; // Khởi tạo đối tượng userData để lưu thông tin kết quả đăng nhập
             let isExist = await checkUserEmail(userEmail); // người dùng nhập đúng với email trong database
             if (isExist) {
                 // user already exists
-                let user = await db.User.findOne({
+                let user = await db.User.findOne({ 
                     attributes: ['email', 'roleId', 'password'], // attributes trong Sequelize tương đương với câu lệnh SELECT trong SQL.
                     where: { email: userEmail }, // Điều kiện tìm kiếm, tìm bản ghi người dùng có email bằng với email đã nhập.
                     raw: true
@@ -17,24 +17,26 @@ let handleUserLogin = (userEmail, password) => {
                 if (user) {
                     // compare password
                     let check = bcrypt.compareSync(password, user.password);
+// Phương thức compareSync sẽ lấy mật khẩu người dùng nhập vào (password) và so sánh nó với mật khẩu đã được mã hóa trong cơ sở dữ liệu (user.password).
+// Nếu hai mật khẩu khớp nhau, compareSync sẽ trả về true.
+// Nếu hai mật khẩu không khớp, compareSync sẽ trả về false.
 // bcrypt.compareSync(password, user.password): So sánh mật khẩu người dùng nhập vào (password) với mật khẩu đã được mã hóa trong cơ sở dữ liệu (user.password).
                     if (check) {
-                        userData.errCode = 0;
+                        userData.errCode = 0;   // Đăng nhập thành công
                         userData.errMessage = 'OK';
 
                         delete user.password; // Chỉnh lại để xoá password từ user
-                        userData.user = user;
+                        userData.user = user; // Lưu thông tin người dùng vào userData
                     } else {
                         userData.errCode = 3;
                         userData.errMessage = 'Wrong password';
                     }
                 } else {
-                    userData.errCode = 2;
+                    userData.errCode = 2; // Người dùng không tồn tại trong hệ thống
                     userData.errMessage = "User's not found";
                 }
             } else {
-                // return error
-                userData.errCode = 1;
+                userData.errCode = 1;  // Email không tồn tại trong hệ thống
                 userData.errMessage = "Your Email isn't exist in your system. please try other email!";
             }
             resolve(userData);
@@ -44,7 +46,6 @@ let handleUserLogin = (userEmail, password) => {
         }
     });
 };
-
 
 let checkUserEmail = (userEmail) => {
     return new Promise( async(resolve, reject) => {
@@ -63,7 +64,35 @@ let checkUserEmail = (userEmail) => {
             reject(e);
         }
     })
+};
+
+let getAllUsers = (userId) => {
+    return new Promise( async (resolve, reject) => {
+        try{
+            let users = '';
+            if(userId === 'ALL'){
+                users = await db.User.findAll({
+                    attributes: {
+                        exclude: ['password']
+                    }
+                });
+            }
+            if(userId && userId !== 'ALL'){
+                users = await db.User.findOne({
+                    where: { id: userId },
+                    attributes: {
+                        exclude: ['password']
+                    }
+                });
+            }
+            resolve(users)
+        }catch(e){
+            reject(e);
+        }
+    })
 }
+
 module.exports = {
-    handleUserLogin: handleUserLogin
+    handleUserLogin: handleUserLogin,
+    getAllUsers: getAllUsers
 }
